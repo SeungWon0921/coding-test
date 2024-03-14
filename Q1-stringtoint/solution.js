532f880549f2dfc166f0c5ee1db5aed2f197c730
zockq1 <zockq1@gmail.com>
2024-03-14 16:47:11 +0900

    fix: 변수명 변경

--- Q1-stringtoint/solution.js
+++ Q1-stringtoint/solution.js
@@ -4,13 +4,13 @@ function solution(s) {
   const isNegative = s[0] === "-";
   const isSign = s[0] === "-" || s[0] === "+";
 
-  const digits = [...s.slice(isSign ? 1 : 0)].map((char) => {
-    const code = char.charCodeAt(0) - "0".charCodeAt(0);
-    return code >= 0 && code <= 9 ? code : NaN;
+  const digitList = [...s.slice(isSign ? 1 : 0)].map((char) => {
+    const digit = char.charCodeAt(0) - "0".charCodeAt(0);
+    return digit >= 0 && digit <= 9 ? digit : NaN;
   });
 
-  const result = digits.reduce((acc, curr, index, array) => {
-    if (isNaN(curr)) {
+  const result = digitList.reduce((acc, curr, index, array) => {
+    if (Number.isNaN(curr)) {
       array.splice(1);
       return NaN;
     }
